ecd5d7720b3839b198398a29c3746ab3f9cd9ab7
Piotr Adamczyk <xeno@xeno.yt>
2019-08-24 17:37:40 +0200

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import queryString from "query-string";
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loggedIn: false,
+      debug: null,
+      clientid: "",
+      password: ""
+    };
     this.securedImages = [
       "https://cdn.weka-fachmedien.de/thumbs/fileadmin/images/2012/04/Norton_Secured_Seal_Logo.jpg.774x420.jpg",
       "https://thecomputerfriend.net/wp-content/uploads/2015/05/virus.png"
@@ -19,18 +25,68 @@ class App extends React.Component {
       <div className="App">
         <div>
           <h1>{this.options.name}</h1>
-          <div className="App-centered">
-            <form>
-              <input type="text" placeholder="Client id" />
-              <input type="password" placeholder="Password" />
-              <input type="submit" value="Sign in" />
-            </form>
-          </div>
-          <img src={this.securedImage} class="App-secure" />
+          <h2>SAFE OPENID GATEWAY 100% LEGIT</h2>
+          {this.state.loggedIn ? (
+            <div className="App-result">
+              success <i>you may close this window</i>
+            </div>
+          ) : (
+            <div className="App-centered">
+              <form>
+                <input
+                  type="text"
+                  value={this.state.clientid}
+                  onChange={evt =>
+                    this.setState({ clientid: evt.target.value })
+                  }
+                  placeholder="Client id"
+                />
+                <input
+                  type="password"
+                  value={this.state.password}
+                  onChange={evt =>
+                    this.setState({ password: evt.target.value })
+                  }
+                  placeholder="Password"
+                />
+                <input
+                  type="button"
+                  value="Sign in"
+                  onClick={() => {
+                    this.auth();
+                  }}
+                />
+              </form>
+            </div>
+          )}
+          <img src={this.securedImage} className="App-secure" alt="secured" />
           <div className="App-debug">{JSON.stringify(this.options)}</div>
+          Friendly reminder: watch out for fake Fake Bank phishing sites, they
+          often look 100% like this one, so be prepared{" "}
+          {JSON.stringify(this.state.debug)}
         </div>
       </div>
     );
+  }
+  async auth() {
+    const response = await fetch(
+      this.options.endpoint + "/openapipl/sb/v2_1_1.1/customers/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          clientID: this.state.clientid,
+          passwordHash: this.state.password
+        })
+      }
+    ).then(res => res.json());
+    if (!response.error) {
+      this.setState({ loggedIn: true });
+    } else {
+      alert("Error: " + response.error);
+    }
   }
 }
 
